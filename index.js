29eafe761df4d8c717187c2cc8747115c5c2441c
duro <davidduro2002@gmail.com>
2024-08-27 13:50:32 +0200

    Starting with new library

--- index.js
+++ index.js
@@ -5,11 +5,45 @@ const {ETwitterStreamEvent} = require('twitter-api-v2');
   
 
 
-async function user_id() {
-    // const user = await dev_client.v2.userByUsername('IbaiLLanos');
-    // const userId = user.data.id;
-    // console.log(`ID del usuario IbaiLLanos: ${userId}`);
+// async function user_data(nick_name) {
+//   // const user = await dev_client.v2.userByUsername(nick_name);
+//   // const userId = user.data;
+//   const response = await dev_client.users.findUsersById({
 
+//     "ids": [
+
+//         "2754746065"
+
+//     ],
+
+//     "expansions": [
+
+//         "most_recent_tweet_id"
+
+//     ]
+
+//   });
+
+  
+
+//   console.log("response", JSON.stringify(response, null, 2));
+
+// }
+
+async function replyToTweet(tweetId, message) {
+  try {
+    // Publica un tweet en respuesta a otro tweet usando la API v2
+    const tweet = await user_client.v2.tweet({
+      text: message,
+      reply: {
+        in_reply_to_tweet_id: tweetId, // ID del tweet al que estás respondiendo
+      },
+    });
+
+    console.log('Tweet enviado:', tweet);
+  } catch (error) {
+    console.error('Error al enviar el tweet:', error);
+  }
 }
 
 async function simple_tweet(tweet) {
@@ -31,7 +65,8 @@ async function main() {
     const userId = user.data.id;
     console.log(`Tu ID de usuario: ${userId}`);
 
-    simple_tweet("Judías")
+    //replyToTweet('1828373871512207397', "Queso")
+    // user_data("IbaiLLanos")
 };
 
 main();
